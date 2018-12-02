85f3bad6ecb90fe8c42211cb9f02a370a972b1f2
tsmilgius <tsmilgius@gmail.com>
2018-12-02 16:40:00 +0200

    state to props

---
+++ src/SeasonsDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SeasonDisplay = (props) => {
+  console.log(props.lat);
+  return(
+    <div></div>
+  );
+};
+
+export default SeasonDisplay;
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonsDisplay';
+
+class App extends React.Component {
+ 
+  state = {  lat: null, errorMessage: '' };
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  componentDidUpdate(){
+    console.log('My component did update');
+  }
+
+  render(){
+    if (this.state.errorMessage && !this.state.lat) {
+      return(
+        <div>Error: {this.state.errorMessage}</div>
+      );
+    }
+    if (this.state.lat && !this.state.errorMessage) {
+      return(
+        <SeasonDisplay lat={this.state.lat}/>
+      );
+    }
+    if(!this.state.errorMessage && !this.state.lat) {
+      return(
+        <div> Loading</div>
+      );
+    }
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
